542409e726000b03cc544712b5ce22e00e673073
Hikigaya <otakusekainet@gmail.com>
2016-09-17 02:03:00 +0300

    Azir IMPROVED

--- MakeYourChampionOP/Program.cs
+++ MakeYourChampionOP/Program.cs
@@ -19,7 +19,7 @@ namespace MakeYourChampionOP
 
         private static void OnLoad(EventArgs args)
         {
-            Config = new Menu("MAKE YOUR CHAMPION OP","MAKE YOUR CHAMPION OP",true);
+            Config = new Menu("MAKE YOUR CHAMPION OP", "MAKE YOUR CHAMPION OP", true);
             {
                 Config.AddItem(new MenuItem("inject", "Inject ?")).SetValue(false);
                 Config.AddItem(new MenuItem("trigger.azir", "Trigger Azir Q Every Solider Auto Attack")).SetValue(false);
@@ -28,10 +28,12 @@ namespace MakeYourChampionOP
             Config.AddToMainMenu();
 
             Game.PrintChat("If you wanna inject. Just set Inject to on and press f5. enjoy your op champion");
+            Game.PrintChat("#FREEKARL");
 
             Hacks.UnsafeCasting = Config.Item("inject").GetValue<bool>();
             Obj_AI_Base.OnProcessSpellCast += OnProcessSpellCast;
         }
+
 
         private static void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
@@ -43,29 +45,43 @@ namespace MakeYourChampionOP
             if (ObjectManager.Player.ChampionName == "Graves" && args.SData.Name.ToLower().Contains("attack")
                 && Config.Item("inject").GetValue<bool>())
             {
-                ObjectManager.Player.Spellbook.CastSpell(SpellSlot.E, Game.CursorPos);
+                ObjectManager.Player.Spellbook.CastSpell(SpellSlot.E, Game.CursorPos, true);
             }
 
             if (ObjectManager.Player.ChampionName == "Azir")
             {
                 if (Config.Item("inject").GetValue<bool>())
                 {
-                    if (args.SData.Name ==
-                    ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.Name || args.SData.Name.ToLower().Contains("attack"))
+                    if (Config.Item("trigger.azir").GetValue<bool>())//
                     {
-                        ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Q, Game.CursorPos);
+                        if (args.SData.Name == ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.Name
+                            || args.SData.Name.ToLower().Contains("attack"))
+                        {
+                            var enemy = TargetSelector.GetTarget(825, TargetSelector.DamageType.Magical);
+                            if (enemy != null)
+                            {
+                                ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Q, enemy.Position, true);
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (args.SData.Name == ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.Name)
+                        {
+                            var enemy = TargetSelector.GetTarget(825, TargetSelector.DamageType.Magical);
+                            if (enemy != null)
+                            {
+                                ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Q, enemy.Position, true);
+                            }
+                        }
                     }
                 }
                 else
                 {
-                    if (args.SData.Name ==
-                    ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).SData.Name)
-                    {
-                        ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Q, Game.CursorPos);
-                    }
+                    return;
                 }
             }
-
         }
     }
 }
